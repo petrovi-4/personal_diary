122a287943b07d83e509789a91359ea2fbc34cee
Сергей Мартынов <40.10.444@gmail.com>
2022-10-14 20:42:53 +0300

    Merge remote-tracking branch 'origin/master'

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -37,8 +37,10 @@ if settings.DEBUG:
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
     '''
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+handler403 = 'core.views.permission_denied'
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
-handler403 = 'core.views.permission_denied'
+
