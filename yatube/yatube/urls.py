325be1123eec9133a0fe6142be976a87d102df96
Сергей Мартынов <40.10.444@gmail.com>
2023-01-10 11:47:58 +0300

    Add static

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -14,14 +14,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    '''
-    import debug_toolbar
-    import mimetypes
-    mimetypes.add_type('application/javascript', '.js', True)
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
-    '''
+    #import debug_toolbar
+    #urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
